4fae1d35b035e623aeb19ffd534f738e0f85b4a6
aleishis <mecedesval@gmail.com>
2025-11-26 20:27:28 -0700

    Se agregan estilos al proyecto y se generalizan los outputs de las funciones

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/minimo_comun_divisor', methods=['GET', 'POST'])
+@app.route('/maximo_comun_divisor', methods=['GET', 'POST'])
 def minimo_comun_divisor():
     
     if request.method == 'POST':
@@ -16,10 +16,24 @@ def minimo_comun_divisor():
         numero1 = int(request.form.get('numero1'))
         numero2 = int(request.form.get('numero2'))
         
-        lcd = mcd.mcd(numero1, numero2)
+        resultado = mcd.mcd(numero1, numero2)
         
-        return render_template('result_mcd.html', lcd = lcd)
+        return render_template('result', resultado = resultado)
     return render_template('index.html')
+
+@app.route('/euclides', methods=['POST', 'GET'])
+def algoritmo_euclides():
+    
+    if request.method == 'POST':
+        
+        numero1 = int(request.form.get('numero1'))
+        numero2 = int(request.form.get('numero2'))
+        
+        resultado = euclides.euclides(numero1, numero2)
+        
+        return render_template('result.html', resultado = resultado)
+    return render_template('index.html')
+
     
 
 if __name__ == '__main__':
